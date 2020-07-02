eb9fa105da65b70e819e06d54db5b5e3d845d66e
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2020-07-03 00:35:43 +0900

    initial commit

---
+++ src/main/java/com/kawakawaryuryu/samplefileapi/SampleFileApiApplication.java
@@ -0,0 +1,13 @@
+package com.kawakawaryuryu.samplefileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleFileApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SampleFileApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kawakawaryuryu/samplefileapi/SampleFileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawakawaryuryu.samplefileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleFileApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
